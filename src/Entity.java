618a8ac4915ab36d003d81d5d0604b89dc986e64
Chad Ross <rosschad98@icloud.com>
2019-01-23 23:56:13 -0800

    Move the code for entity location/rotation updates into the entity class to make it reusable

--- src/Entity.java
+++ src/Entity.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
 
+import com.studiohartman.jamepad.ControllerState;
+
 /**
  * This class manages {@code Entity} objects. These objects
  * represent both {@code Player} and {@code Enemy} objects.
@@ -45,5 +47,67 @@ abstract class Entity {
 		panel.add(label);
 		panel.revalidate();
 		panel.repaint();
+	}
+	
+	/**
+	 * Checks the controller state against the location that an entity
+	 * is attempting to travel to to determine if the move is legal. If
+	 * the move is legal, update their location to that location. Else,
+	 * keep the entity where it is.
+	 * 
+	 * @param currState
+	 * @param character
+	 * @param currRoom
+	 */
+	protected void updateEntityLocation(ControllerState currState, RotateLabel character, Room currRoom) {
+		if(currState.leftStickMagnitude >= SpaceQuest.minMagnitude) {
+			System.out.println(character.getLocation());
+			
+			// Determine what direction to send the player.
+			// For reference, 0 is right, PI/2 is upward,
+			// PI is left, and -PI/2 is downward.
+			double currAngle = Math.toRadians(currState.leftStickAngle);
+
+			//if left stick magnitude is over one, change it to one to represent that player is at maximum speed
+			double lsmag = currState.leftStickMagnitude;
+			if (lsmag > 1.0)
+				lsmag = 1.0;
+
+			// The formula below determines what percentage out
+			// of 5 pixels the player should move in the X and Y
+			// directions. The result is approximated since it
+			// must be type int. Add for newX because x increases
+			// from left to right and subtract for newY because y
+			// increases from top to bottom.
+			int newX = character.getLocation().x + (int)Math.round((Math.cos(currAngle)*5*lsmag*2));
+			int newY = character.getLocation().y - (int)Math.round((Math.sin(currAngle)*5*lsmag*2));
+
+			// Check to see if the player will stay inside of the
+			// bounds of the map. If so, update their location.
+			// Otherwise, keep their location the same.
+			if(currRoom.checkRoomBounds(character, new Point(newX, newY))) {
+				// Update the JLabel which represents the character.
+				character.setLocation(newX, newY);
+			} else if (currRoom.checkRoomBounds(character, new Point(character.getLocation().x, newY))) {
+				character.setLocation(character.getLocation().x, newY);
+			} else if (currRoom.checkRoomBounds(character, new Point(newX, character.getLocation().y))) {
+				character.setLocation(newX, character.getLocation().y);
+			} else {
+				character.setLocation(character.getLocation().x, character.getLocation().y);
+			}
+		}
+	}
+	
+	/**
+	 * Sets the entity's rotation equal to the angle of the right stick.
+	 * 
+	 * @param currState
+	 * @param character
+	 */
+	protected void updateEntityRotation(ControllerState currState, RotateLabel character) {
+		if(currState.rightStickMagnitude >= SpaceQuest.minMagnitude) {
+			character.setRotation(currState.rightStickAngle);
+			character.setLocation(character.getLocation().x, character.getLocation().y);
+		}
 	}
 }
--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,40 @@
 import javax.swing.*;
+
+import com.studiohartman.jamepad.ControllerState;
+
 import java.awt.*;
 
 public class Player extends Entity {
+	private RotateLabel character;
+	
 	Player(int initialHealth, int initialStrength, int hitbox, JPanel panel, ImageIcon image, Rectangle location) {
 		createEntity(initialHealth, initialStrength, hitbox);
 		drawEntity(panel, image, location);
+	}
+	
+	Player(RotateLabel character) {
+		this.character = character;
+	}
+	
+	/**
+	 * Checks the controller state against the location that a player
+	 * is attempting to travel to to determine if the move is legal. If
+	 * the move is legal, update their location to that location. Else,
+	 * keep the player where they are.
+	 * 
+	 * @param currState
+	 * @param currRoom
+	 */
+	public void updatePlayerLocation(ControllerState currState, Room currRoom) {
+		updateEntityLocation(currState, character, currRoom);
+	}
+	
+	/**
+	 * Sets the player's rotation equal to the angle of the right stick.
+	 * 
+	 * @param currState
+	 */
+	public void updatePlayerRotation(ControllerState currState) {
+		updateEntityRotation(currState, character);
 	}
 }
